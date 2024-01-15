cebf30a5c02270be6b5d1bc873e9ec70aad47ec4
Arina Nasri <nasri.arina@gmail.com>
2024-01-15 23:22:57 +0000

    did some of ch. 4

--- projects/eloquent-javascript/chapter-04/exercises.js
+++ projects/eloquent-javascript/chapter-04/exercises.js
@@ -62,31 +62,86 @@ function reverseArrayInPlace(array){
 // arrayToList /////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function arrayToList() {
-
+function arrayToList(array) {
+  let rest = null;
+  for (let i = array.length-1; i >= 0; i--){
+    rest = {value: array[i], rest: rest}; // reassigning rest to an object
+  }
+  return rest;
 }
-
+/*
+// invoke arrayToList([10, 20, 30])
+  // rest = null
+  // for loop
+    // i = 2
+      // rest = {value: 30, rest: null}
+    // i = 1
+      // rest = {value: 20, rest: {value: 30, rest: null}}
+    // i = 0
+      // rest = {value: 10, rest: {value: 20, rest: {value: 30, rest: null}
+    // return {value: 10, rest: {value: 20, rest: {value: 30, rest: null}
+*/
 ////////////////////////////////////////////////////////////////////////////////
 // listToArray /////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-
-function listToArray() {
-
+/* let list1 = {
+  value: 10,
+  rest: {
+    value: 20,
+    rest: {
+      value: 30,
+      rest: null
+    }
+  }
 }
+*/
+
+function listToArray(list, array=[]) {
+  // base
+  // once this base is found, recursion will stop
+  if (list.rest === null){
+    array.push(list.value);
+    return array;
+  }
+  // recursion
+    // how to update array
+    array.push(list.value);
+
+  return listToArray(list.rest, array);
+}
+// console.log(listToArray(list1)); // => [10, 20, 30]
+  // BASE? NO
+  // RECURSION
+    // array.push(list.value) => push 10 into array => [10]
+    // return listToArray(list.rest, [10])
+      // return listToArray({value: 20, rest: {value: 30, rest: null}, [10])
+        // BASE? NO
+        // RECURSION
+          // array.push(list.value) => push 20 into array => [10, 20]
+          // return listToArray(list.rest, [10, 20])
+            // return listToArray(rest: {value: 30, rest: null}, [10, 20])
+              // BASE? YES
+                // array.push(30) => [10, 20, 30]
+                // return [10, 20, 30]
 
 ////////////////////////////////////////////////////////////////////////////////
 // prepend /////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function prepend() {
-
+// takes an element and a list
+// creates a new list that adds the element to the front of the input list
+function prepend(element, list) {
+  
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // nth /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function nth() {
+// takes a list and a number
+// returns the element at the given position in the list (with 0 referring to the first element)
+// undefined when there is no such element
+function nth(list, num) {
 
 }
 
@@ -94,8 +149,38 @@ function nth() {
 // deepEqual ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function deepEqual() {
-
+function deepEqual(x, y) {
+  // determine if x and y are NOT objects
+  // ex: deepEqual(1, 1)
+  if (typeof x !== "object" && typeof y !== "object"){
+    return x === y; // aka return the result of whatever this evaluates to
+  }
+  // determine if x OR y is not an object
+  // ex: deepEqual({a: 1}, "a")
+  if (typeof x !== "object" || typeof y !== "object"){
+    return false;
+  }
+  // now we are dealing with 2 objects
+  // create arrays of each inputs keys
+  let xKeys = Object.keys(x); // stores values of keys in arrays
+  let yKeys = Object.keys(y) // stores values of keys in arrays
+  // very quickly catches if the arrays have different lengths meaning the objects obvi aren't the same
+  // ex: deepEqual({a: 1, b: 2}, {a: 1})
+  if (xKeys.length !== yKeys.length){
+    return false;
+  }
+  // iterate to determine if array keys match and value at keys match
+  for (let i = 0; i < xKeys.length; i++){
+    // determine if the current key is NOT included in yKeys
+    // aka if yKeys is now ["a"] is "a" in xKeys
+    // invoking deepEqual function on the value at the particular key we are on
+    if (!yKeys.includes(xKeys[i]) || !deepEqual(x[xKeys[i]], y[xKeys[i]])){
+    //                                deepEqual(x["a"], y["a"])
+    //                                deepEqual(x[1], y[1]) is this truthy? ya
+      return false
+    }
+  }
+  return true;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
